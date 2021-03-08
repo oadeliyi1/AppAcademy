2178898f8b84cd51a026ba0d39eed035775608f4
oadeliyi1 <oriyomiadeliyi@gmail.com>
2021-03-08 18:49:39 -0500

    Complete notes

--- week1/Monday/lectures/notes.js
+++ week1/Monday/lectures/notes.js
@@ -1,11 +1,48 @@
 //Numbers Type Demo Video
 //follow PEMDAS
-
+/*
 console.log(22 % 5);
-
+*/
 
 /* Boolean Type Lecture */
+/*
+ console.log(!true);
 
-console.log(!true);
+ console.log(2 - "hey");
+*/
 
-console.log(2 - "hey");
+//Variables video
+//-inititialization
+//-declaration
+
+let age = 0;
+age = age + 5;
+age += 5; //is the same as above
+age++;
+console.log(age);  //this will come out as 0 becuase we need to reassign age variable to change its value
+
+
+let dishes = 'plate'; //initialization
+let dishes; //declaration
+dishes = 'bowl'; //assignment
+
+
+
+//COMPARISON OPERATORS
+
+let number = 10;
+let isEven = number % 2 === 0;
+console.log(isEven); //will print true
+
+// //loose equality == will coerce types to match; AVOID
+// //strict equalty === has to be the same type
+
+console.log("A" < 'a'); //capitalized letters are considered less than lower case
+
+console.log(9 / 2);
+
+console.log(25 % 7);
+
+let three = 'three';
+let two = 2;
+console.log(three / two);
